27d1eccbf7bdb9458b53a3c4f2d1b8f1ba6dd8ca
Harsh <hrao9395@gmail.com>
2020-04-12 14:24:51 -0400

    2nd

---
+++ ForLoop.java
@@ -0,0 +1,35 @@
+package JavaBasics;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		for (int i = 1; i<=20; i++) {
+			
+			for (int b=1; b <=i; b++) {
+				
+				System.out.print("* ");
+				
+			}
+				
+				System.out.println();
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
