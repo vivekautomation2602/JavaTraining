3ad5c1cc5d672e4e4398f640daa05e9f6933dd43
Harsh <hrao9395@gmail.com>
2020-04-12 14:28:05 -0400

    new file

---
+++ IfelseCondition.java
@@ -0,0 +1,86 @@
+package JavaBasics;
+
+public class IfelseCondition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+	
+ int a = 40;
+ 
+ int b = 30 ;
+	
+ if (a<b)
+ {
+	
+	System.out.println( "a is greater then b");
+	
+ }
+	
+ else {
+	System.out.println("b is greter then a");
+ }
+	
+	// comparison Operator    
+	// < > == <= >= != 
+ 
+ 
+ int c = 40 ;
+ int d = 40;
+ 
+ 
+ if (c==d) {
+	System.out.println("c is equal d");
+	 }
+ 
+	
+	else {
+	System.out.println("a is not equal to b");
+	}
+
+
+// write a logic to find out the highest number 
+ 
+   int a1 = 100;
+   int b1 = 200;
+   int c1 = 300;
+  
+   // Nested If Condition 
+   
+   // false & false = false 
+   
+   if (a1>b1 & a1>c1)  {
+System.out.println("a1 is greaterst");
+   }
+   
+   else if (b1>c1) {
+	   System.out.println("b1 is greatest");
+   }
+   
+   else {
+	   System.out.println("c1 is greatest");
+   }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+	}
+
+
+}
+
+	
+	
+	
+	
+
