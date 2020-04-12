ce1fcad4eb8443544a4187f3e30704c5bd0892db
Umang tiwari <umangtiwari@qainfotech.com>
2020-04-12 23:48:09 +0530

    new files

---
+++ DefaulConstructor.java
@@ -0,0 +1,11 @@
+package test.constructor;
+
+public class DefaulConstructor {
+	DefaulConstructor(){System.out.println("Bike is created");}  
+	//main method  
+	public static void main(String args[]){  
+	//calling a default constructor  
+		DefaulConstructor b=new DefaulConstructor();  
+	}  
+
+}
---
+++ Student.java
@@ -0,0 +1,21 @@
+package test.constructor;
+
+public class Student {
+	int id;
+	String name;
+
+//method to display the value of id and name  
+	void display() {
+		System.out.println(id + " " + name);
+	}
+
+	public static void main(String args[]) {
+//creating objects  
+		Student s1 = new Student();
+		Student s2 = new Student();
+//displaying values of the object  
+		s1.display();
+		s2.display();
+	}
+
+}
